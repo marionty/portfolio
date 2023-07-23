6ac1be2385b6fb2017c5d7d987256def8fc133e1
marionty <marion.tytgat@gmail.com>
2023-07-23 10:45:24 +0200

    commit

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -5,6 +5,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import Confetti from "react-dom-confetti";
+
+
 import "../Styles/Styles.css";
 
 
